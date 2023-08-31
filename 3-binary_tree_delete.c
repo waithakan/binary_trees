6b2ad5caa55ca4bf0831f424c0dca1f58a51c000
Your name <you@example.com>
2023-08-31 01:26:31 -0700

    a function that deletes an entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: binary tree to be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
